92c87f1d6b240a4a52f170bc12b12d5a38cf8c9a
Jaxon Desmarais <jaxon.desmarais@maverik.com>
2025-05-15 15:05:05 -0600

    Adding my own hook

--- src/your_pkg/your_module.py
+++ src/your_pkg/your_module.py
@@ -1,14 +1,14 @@
 import re
-from pathlib import Path
 
 
-pattern = r'(?i)verify\s*=\s*false'
+pattern = r"(?i)verify\s*=\s*false"
+
 
 def check_string_in_file(filename):
     try:
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             for line in file:
-                if re.findall(pattern,line):
+                if re.findall(pattern, line):
                     print(line.upper())
                     failure = True
                     break
@@ -17,9 +17,10 @@ def check_string_in_file(filename):
     except FileNotFoundError:
         failure = False
     if failure:
-        print(f"SSL Verification Disabled, FAIL")
+        print("SSL Verification Disabled, FAIL")
     else:
-        print(f"SSL Verification Enabled, PASS")
+        print("SSL Verification Enabled, PASS")
     return 1 if failure else 0
+
 
 # check_string_in_file('pre-commit-workshop/test.py')
